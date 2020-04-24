b680b8a523b53d3e269fc227e088bff18e0eab01
Recruit <andisiwe.nkwana@umuzi.org>
2020-04-24 13:35:07 +0200

    first commit

--- person.js
+++ person.js
@@ -1,15 +1,15 @@
-class person{
+class Person{
     constructor(name, age, gender, intrests){
         this.name = name
         this.age = age
         this.gender = gender
         this.intrests = intrests
     }
-    Hello(){
+    hello(){
         console.log(`Hello my name is ${this.name} i'm ${this.age} years old and my intrests are ${this.intrests}.`)
     }
     
 }
-let Person = new person('Ryan',30,'male',['being a hardarse','agile','ssd hard drives'])
-let greeting = Person.Hello()
+let person = new Person('Ryan',30,'male',['being a hardarse','agile','ssd hard drives'])
+let greeting = person.hello()
 console.log(greeting)
