b62d7407a8b39226d463be4bece0225b3497cbd7
Recruit <andisiwe.nkwana@umuzi.org>
2020-05-15 09:13:25 +0200

    resubmission

--- src/person.js
+++ src/person.js
@@ -1,14 +1,24 @@
-class Person{
-    constructor(name, age, gender, interests){
-        this.name = name
-        this.age = age
-        this.gender = gender
-        this.interests = interests
-    }
-    hello(){
-        return('Hello, my name is '+ this.name  + ' and I am ' + this.age + ' years old. My interests are ' + this.interests[0] + ',' + this.interests[1] + ' and' +  this.interests[2]);
-    }
-    
+class Person {
+  constructor(name, age, gender, interests) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+  }
+  hello() {
+    return (
+      "Hello, my name is " +
+      this.name +
+      " and I am " +
+      this.age +
+      " years old. My interests are " +
+      this.interests[0] +
+      ", " +
+      this.interests[1] +
+      " and " +
+      this.interests[2] + "."
+    );
+  }
 }
- 
-module.exports =Person 
+
+module.exports = Person;
